2896c58ee9eed06dd1c5df29021f237499f251b1
Andrew Druffner <andrew@nomstock.com>
2015-02-06 07:39:11 -0800

    archive admin changes before deployment

--- admin/limit-logins-plugin-reset.sql
+++
@@ -1,5 +0,0 @@
-
--- I locked myself out testing this thing, what do I do?
--- ref:https://wordpress.org/plugins/limit-login-attempts/faq/
-UPDATE wp_options SET option_value = '' WHERE option_name = 'limit_login_lockouts'
-
