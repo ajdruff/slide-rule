6574ac77ad1f3b8613a4bfde2962b0b3e39e6e6c
Andrew Druffner <andrew@nomstock.comexit>
2015-02-17 14:35:27 -0800

    added skip worktree script

---
+++ admin/git-skip-worktree.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+#################
+# git-skip-worktree.sh
+#
+#
+# Description
+#
+#  Usage:
+# ./git-skip-worktree.sh
+#
+#
+# @author <user@example.com>
+#
+#################
+ 
+skip_worktree='--skip-worktree';
+options=':n'
+while getopts $options option
+do
+    case $option in
+        n  )    skip_worktree='--no-skip-worktree';;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
+
+#get the directory this file is in
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#get its parent directory pat
+DIR_PARENT=$(dirname $DIR);
+#read common variables for all bash scripts
+#source "${DIR%%/}/config-bash.conf";
+
+#git update-index --skip-worktree "conf-*";
+command="git update-index ${skip_worktree} ${DIR%%/}/config-*";
+eval $command;
+command="git update-index ${skip_worktree} ${DIR%%/}/change-password.sql";
+eval $command;
+command="git update-index ${skip_worktree} ${DIR%%/}/create-new-admin-user.sql";
+eval $command;
+
+
+# list all those being skipped
+echo 'git will skip the following files';
+git ls-files -v . | grep ^S
+
